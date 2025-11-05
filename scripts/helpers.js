873533c5589b50db8f74763750d78ade18cf2302
wirewolx <oleg9696125@gmail.com>
2025-11-05 21:55:21 +0000

    Delete scripts directory

--- scripts/helpers.js
+++
@@ -1,4 +0,0 @@
-function $(sel, root = document) { return root.querySelector(sel); }
-function $$(sel, root = document) { return [...root.querySelectorAll(sel)]; }
-function clamp(v, min, max) { return Math.min(max, Math.max(min, v)); }
-function isHex(v) { return /^#([0-9a-f]{3}|[0-9a-f]{6})$/i.test(v); }
